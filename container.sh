4a1d1c96693059d77b3fff154e60ecbcfc920360
Eric Zhang <ezhang887@gmail.com>
2019-12-10 02:57:15 -0600

    Cleanup

--- container.sh
+++ container.sh
@@ -40,33 +40,33 @@ mount -t proc proc proc/
 cd -
 
 # setup networking
-VETH="veth${CONTAINER_NAME}"
-VPEER="vpeer${CONTAINER_NAME}"
+VETH="veth$CONTAINER_NAME"
+VPEER="vpeer$CONTAINER_NAME"
 VETH_ADDR="10.200.1.1"
 VPEER_ADDR="10.200.1.2"
 IFACE="wlp8s0"
 
 ip netns add $CONTAINER_NAME
 
-ip link add ${VETH} type veth peer name ${VPEER}
-ip link set ${VPEER} netns $CONTAINER_NAME
-ip addr add ${VETH_ADDR}/24 dev ${VETH}
-ip link set ${VETH} up
-ip netns exec $CONTAINER_NAME ip addr add ${VPEER_ADDR}/24 dev ${VPEER}
-ip netns exec $CONTAINER_NAME ip link set ${VPEER} up
+ip link add $VETH type veth peer name $VPEER
+ip link set $VPEER netns $CONTAINER_NAME
+ip addr add $VETH_ADDR/24 dev $VETH
+ip link set $VETH up
+ip netns exec $CONTAINER_NAME ip addr add $VPEER_ADDR/24 dev $VPEER
+ip netns exec $CONTAINER_NAME ip link set $VPEER up
 ip netns exec $CONTAINER_NAME ip link set lo up
-ip netns exec $CONTAINER_NAME ip route add default via ${VETH_ADDR}
+ip netns exec $CONTAINER_NAME ip route add default via $VETH_ADDR
 echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -P FORWARD DROP
 iptables -F FORWARD
 iptables -t nat -F
-iptables -t nat -A POSTROUTING -s ${VPEER_ADDR}/24 -o ${IFACE} -j MASQUERADE
-iptables -A FORWARD -i ${IFACE} -o ${VETH} -j ACCEPT
-iptables -A FORWARD -o ${IFACE} -i ${VETH} -j ACCEPT
+iptables -t nat -A POSTROUTING -s $VPEER_ADDR/24 -o $IFACE -j MASQUERADE
+iptables -A FORWARD -i $IFACE -o $VETH -j ACCEPT
+iptables -A FORWARD -o $IFACE -i $VETH -j ACCEPT
 
 # dns
 rm $PWD/image/etc/resolv.conf
 echo "nameserver 8.8.8.8" > $PWD/image/etc/resolv.conf
 
 # unshare in the new network namespace
-ip netns exec ${CONTAINER_NAME} unshare -p -f --mount-proc=$PWD/image/proc chroot image/ /bin/bash
+ip netns exec $CONTAINER_NAME unshare -p -f --mount-proc=$PWD/image/proc chroot image/ /bin/bash
